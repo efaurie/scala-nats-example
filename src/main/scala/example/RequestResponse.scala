37ce15246aa7753507c3d422e93b6bd347d7103f
Eric Faurie <eric.faurie@gmail.com>
2020-01-25 18:59:05 -0600

    Add time calculation

--- src/main/scala/example/RequestResponse.scala
+++ src/main/scala/example/RequestResponse.scala
@@ -7,10 +7,12 @@ import example.model.{Ack, StringMessage}
 import example.nats.Nats
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import scala.util.{Failure, Success, Try}
 
 object RequestResponse extends App {
+  import example.ext._
   import example.model.AckJson._
   import example.model.StringMessageJson._
   implicit val msgSerde: JsonSerde[StringMessage] = JsonSerde[StringMessage]()
@@ -27,19 +29,21 @@ object RequestResponse extends App {
     case Failure(e) => throw e
   }
 
-  // Publish 10 StringMessage's, this will automatically convert StringMessage -> JSON -> UTF8 -> Bytes
-  (1 to 10).map { seqNum =>
-    nats.request[StringMessage, Ack](
-      "ExampleServer.ack", StringMessage(Instant.now, seqNum, s"This is example $seqNum!")
-    ).onComplete { printReply }
+  timeit {
+    val ackFutures: Seq[Future[Any]] = (1 to 10).map { seqNum =>
+      nats.request[StringMessage, Ack](
+        "ExampleServer.ack", StringMessage(Instant.now, seqNum, s"This is example $seqNum!")
+      ).transform { reply => printReply(reply); reply }
+    }
+
+    val replyFutures: Seq[Future[Any]] = (11 to 3000).map { seqNum =>
+      nats.request[StringMessage, StringMessage](
+        "ExampleServer.reply", StringMessage(Instant.now, seqNum, s"This is example $seqNum!")
+      ).transform { reply => printReply(reply); reply }
+    }
+
+    Await.result(Future.sequence(replyFutures ++ ackFutures), Duration.Inf)
   }
 
-  (11 to 20).map { seqNum =>
-    nats.request[StringMessage, StringMessage](
-      "ExampleServer.reply", StringMessage(Instant.now, seqNum, s"This is example $seqNum!")
-    ).onComplete { printReply }
-  }
-
-  // Give it three seconds to drain all messages and complete
-  nats.drainAndClose(3 seconds)
+  nats.drainAndClose(1 seconds)
 }
--- src/main/scala/example/ext/package.scala
+++ src/main/scala/example/ext/package.scala
@@ -4,8 +4,10 @@ import java.nio.charset.StandardCharsets
 
 import io.nats.client.Message
 import play.api.libs.json.{JsValue, Json}
+
 import scala.concurrent.duration._
 import java.time.{Duration => JDuration}
+import java.util.Locale
 
 package object ext {
   implicit def asJavaDuration(d: Duration): JDuration = JDuration.ofNanos(d.toNanos)
@@ -17,5 +19,58 @@ package object ext {
   implicit class RichMessage(val msg: Message) extends AnyVal {
     def asJson: JsValue = Json.parse(new String(msg.getData, StandardCharsets.UTF_8))
     def toHex: String = msg.getData.map("%02x" format _).mkString
+  }
+
+  def timeit(fn: => Unit): Unit = {
+    val start = System.nanoTime
+    fn
+    val stop = System.nanoTime
+    println(s"Time Elapsed: ${(stop - start).nanos.pretty()}")
+  }
+
+  implicit class PrettyPrintableDuration(val duration: Duration) extends AnyVal {
+    /** Selects most appropriate TimeUnit for given duration and formats it accordingly */
+    def pretty(precision: Int = 4): String = {
+      require(precision > 0, "precision must be > 0")
+
+      duration match {
+        case d: FiniteDuration =>
+          val nanos = d.toNanos
+          val unit = chooseUnit(nanos)
+          val value = nanos.toDouble / NANOSECONDS.convert(1, unit)
+
+          s"%.${precision}g %s%s".formatLocal(
+            Locale.ROOT,
+            value,
+            abbreviate(unit),
+            s" ($nanos ns)")
+
+        case Duration.MinusInf => s"-∞ (minus infinity)"
+        case Duration.Inf      => s"∞ (infinity)"
+        case _                 => "undefined"
+      }
+    }
+
+    def chooseUnit(nanos: Long): TimeUnit = {
+      val d = nanos.nanos
+
+      if (d.toDays > 0) DAYS
+      else if (d.toHours > 0) HOURS
+      else if (d.toMinutes > 0) MINUTES
+      else if (d.toSeconds > 0) SECONDS
+      else if (d.toMillis > 0) MILLISECONDS
+      else if (d.toMicros > 0) MICROSECONDS
+      else NANOSECONDS
+    }
+
+    def abbreviate(unit: TimeUnit): String = unit match {
+      case NANOSECONDS  => "ns"
+      case MICROSECONDS => "μs"
+      case MILLISECONDS => "ms"
+      case SECONDS      => "s"
+      case MINUTES      => "min"
+      case HOURS        => "h"
+      case DAYS         => "d"
+    }
   }
 }
